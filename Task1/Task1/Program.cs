cd66516a3c5184262ad2197ccb5cf57cb92ca388
Predator <murad.mammedzade11@gmail.com>
2023-08-22 03:03:19 -0700

    First save

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Task1
+{
+    public static class GeometryHelper
+    {
+        public static void AreaOfFourAngles(double l, double w)
+        {
+            var area = l * w;
+            Console.WriteLine("Area of four angles " + area);
+        }
+        public static void AreaOfThreeAngles(double b, double h)
+        {
+            var area = 1 / 2.0 * b * h;
+            Console.WriteLine("Area of three angles is " + area);
+        }
+        public static void AreaOfCircle(double r)
+        {
+            var area = Math.PI * r * r;
+            Console.WriteLine("Area of circle is " + area);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GeometryHelper.AreaOfFourAngles(7, 4);
+            GeometryHelper.AreaOfThreeAngles(20, 30);
+            GeometryHelper.AreaOfCircle(25);
+        }
+
+    }
+}
